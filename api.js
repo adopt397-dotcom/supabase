2f3fd2060dfdec5f14e7c5a86ffbac605c011e1f
adopt397-dotcom <adopt397@gmail.com>
2026-07-07 15:55:05 -0700

    Refactor login function to global scope

--- api.js
+++ api.js
@@ -1,11 +1,8 @@
-// /member/api.js
-import { MEMBER_API_URL } from './config.js';
-
-export async function login(email, pin) {
-  const response = await fetch(MEMBER_API_URL, {
+window.login = async function(email, pin) {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbxIymT6beN1izBiBXMR95qT19ykaA1wjcmiqwasghvKJanVfSPpTEu68NcLJJy1QWQC/exec', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, pin })
   });
   return response.json();
-}
+};
