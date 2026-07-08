1650b9d30cc82f20efc028c91543634a83eadc72
adopt397-dotcom <adopt397@gmail.com>
2026-07-07 17:10:06 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 window.login = async function(email, pin) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxIymT6beN1izBiBXMR95qT19ykaA1wjcmiqwasghvKJanVfSPpTEu68NcLJJy1QWQC/exec', {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbyAEu_jxPeIVq2iB5jLC2zGq7dtbXCCUe7ZaQ8Yo8zsBkAeHkPrOZ7wwOhVnGdh4eSs/exec', {
     method: 'POST',
     headers: { 'Content-Type': 'text/plain' },
     body: JSON.stringify({ email, pin })
